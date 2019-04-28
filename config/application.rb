e138da96aebc0ef89fe6e44b9990da8de0535c0a
Eduardo Lowensohn <gunchosoft@gmail.com>
2019-04-28 16:14:48 -0300

    bookmark crud3

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,15 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.serve_static_assets = true
+
 
 module Blocmark
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.serve_static_assets = true
+
     config.load_defaults 5.2
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

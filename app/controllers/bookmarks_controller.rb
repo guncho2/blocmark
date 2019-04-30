25e3c81c41309016d1e8e7608ac7e477ae5a65fc
Eduardo Lowensohn <gunchosoft@gmail.com>
2019-04-29 21:15:37 -0300

    email6

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,71 +2,64 @@ class BookmarksController < ApplicationController
 
 
   def show
-     @topic = Topic.find(params[:topic_id])
-      @bookmark = Bookmark.find(params[:id])
-
-
-	  end
-
-
-
-    def index
-
-      @bookmarks = Bookmark.all
-
-      # return a collection of bookmarks
-    end
-
-	  def new
-      puts '>>>>>>>'
-      puts params
-      @topic = Topic.find(params[:topic_id])
-    
-      @bookmark = Bookmark.new
-	  end
-
-	  def edit
-	    # @bookmark = Bookmark.find(bookmark_params)
-      @topic = Topic.find(params[:topic_id])
+    @topic = Topic.find(params[:topic_id])
     @bookmark = Bookmark.find(params[:id])
-	  end
+  end
 
-	  def create
-      puts @bookmark
-      @bookmark = Bookmark.new
+
+
+  def index
+    @bookmarks = Bookmark.all
+  end
+      # return a collection of bookmarks
+
+
+  def new
+    
+    puts params
+    @topic = Topic.find(params[:topic_id])
+
+    @bookmark = Bookmark.new
+  end
+
+  def edit
+    	    # @bookmark = Bookmark.find(bookmark_params)
+    @topic = Topic.find(params[:topic_id])
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+	 def create
+    puts @bookmark
+    @bookmark = Bookmark.new
  #
-      @bookmark.url = params[:bookmark][:url]
- #     @bookmark = @topic.bookmarks.build(bookmark_params)
-    #  @bookmark.user = current_user
- #      @topic = Topic.find(params[:topic_id])
+    @bookmark.url = params[:bookmark][:url]
+ #
 
- #     @bookmark.topic = @topic
+    @topic = Topic.find(params[:topic_id])
 
- @topic = Topic.find(params[:topic_id])
+    @user = current_user
 
- @user = current_user
-
- @bookmark = @topic.bookmarks.build(bookmark_params)
+    @bookmark = @topic.bookmarks.build(bookmark_params)
 
 
- @bookmark.topic = @topic
-@bookmark.user = @user
+    @bookmark.topic = @topic
+    @bookmark.user = @user
 
 
-	      if @bookmark.save
+	  if @bookmark.save
 
-          puts '>>>>> bookmark was saved'
+      puts '>>>>> bookmark was saved'
 
-	        flash[:notice] = "Bookmark was saved."
-	        redirect_to [@topic, @bookmark]
-	      else
+	    flash[:notice] = "Bookmark was saved."
+	    redirect_to [@topic, @bookmark]
+	     else
 
-          puts '>>>>> failed to save bookmark'
+      puts '>>>>> failed to save bookmark'
 
-	        flash[:error] = "There was an error saving the bookmark. Please try again."
-	        render :new
-	      end
-	   end
+	    lash[:error] = "There was an error saving the bookmark. Please try again."
+	      render :new
+	     end
+	  end
 
 	   def update
 	     @bookmark = Bookmark.find(params[:id])
